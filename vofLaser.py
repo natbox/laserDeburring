efba5e5ba3c3048147beceaef6336e5fc8190152
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 18:08:21 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy
 import plotly.express as alpha
 import plotly
 import array as arr
-c=12
+c=20
 a = 0.1*np.ones((c,c))
 beam = np.ones((c,c))
 a[0][0]=0.5
@@ -14,10 +15,11 @@ for z in range(int(c/2),-int(c/2),-1):
   for y in range(-int(c/2),int(c/2),1):
     i=int(y)
     j=int(z)
-    beam[j][i]=(8*(i**2+j**2)/c**3)
+    r=numpy.sqrt(i**2+j**2)
+    beam[j][i]=numpy.exp(-r**2/c**2)
     beam[j][i]=beam[j][i]/beam.max()
 rayf=a*beam;
-beam=100*beam;
+#beam=100*beam;
 # Solid zinc is surrounded by argon and molten 
 # by a laser beam via creating an array
 i=0
@@ -27,7 +29,7 @@ for z in range(1,int(c/2)+1,1):
   for y in range(int(c)-2,int(c/2)-2,-1):
     i=int(y)
     j=int(z)
-    beam[j][i]=0
+    #beam[j][i]=0
 fig = alpha.imshow(beam,text_auto=True)
 fig.show()
 #plotly.offline.plot(fig, filename='alpha.html')
