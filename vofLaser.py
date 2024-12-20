be3af8cc7a55f3db68d2d81264891eadaffc9b0c
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 18:34:24 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -4,7 +4,8 @@ import numpy
 import plotly.express as alpha
 import plotly
 import array as arr
-c=20
+c=14
+wavelength=633;
 a = 0.1*np.ones((c,c))
 beam = np.ones((c,c))
 a[0][0]=0.5
@@ -18,7 +19,15 @@ for z in range(int(c/2),-int(c/2),-1):
     r=numpy.sqrt(i**2+j**2)
     beam[j][i]=numpy.exp(-r**2/c**2)
     beam[j][i]=beam[j][i]/beam.max()
+    beam[j][i]=1/r**2
 rayf=a*beam;
+
+# z0=1;
+# z=0;
+# W0=numpy.sqrt(wavelength*z0/numpy.pi);
+# Wz=W0*numpy.sqrt(1+(z/z0)**2);
+# I ~exp(2r^2/Wz^2)
+
 #beam=100*beam;
 # Solid zinc is surrounded by argon and molten 
 # by a laser beam via creating an array
