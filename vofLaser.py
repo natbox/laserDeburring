bfac088b08a486c88a15d9956d0165c2e0dafafb
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 16:47:26 +0100

    Update and rename ArZnMixture.py to vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -3,21 +3,30 @@ import numpy as np
 import plotly.express as alpha
 import plotly
 import array as arr
-c=9
+c=10
 a = 0.1*np.ones((c,c))
+beam = np.ones((c,c))
 a[0][0]=0.5
 b = np.arange(c,0,-1)
 i=0
 j=0
-# Solid zinc is surrounded by argon and the metal is dissolved by a mixed process gas
+# Solid zinc is surrounded by argon and the metal is dissolved by a mixed process beam
 # creating array
 k = arr.array('i', [1, 2, 3])
 # iterating and printing each item
-for z in range(1,6,1):
-  for y in range(7,2,-1):
+for z in range(1,c-3,1):
+  for y in range(c-3,1,-1):
     i=int(y)
     j=int(z)
     a[j][i]=0.9
-fig = alpha.imshow(a,text_auto=True)
+i=0
+j=0
+for z in range(int(c/2),-int(c/2),-1):
+  for y in range(-int(c/2),int(c/2),1):
+    i=int(y)
+    j=int(z)
+    beam[j][i]=((i+j)/c)**2
+rayf=a*beam;
+fig = alpha.imshow(rayf,text_auto=True)
 fig.show()
 #plotly.offline.plot(fig, filename='alpha.html')
