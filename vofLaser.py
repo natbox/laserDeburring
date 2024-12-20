8325677a7bd9e872ca8b718476e468925d98276f
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 17:08:44 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -18,7 +18,7 @@ for z in range(0,int(c/2)+1,1):
   for y in range(int(c)-1,int(c/2)-1,-1):
     i=int(y)
     j=int(z)
-    a[j][i]=0.9 # alpha of zinc
+    a[j][i]=0.9 # alpha
 i=0
 j=0
 for z in range(int(c/2),-int(c/2),-1):
