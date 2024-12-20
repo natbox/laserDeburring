fe031b570b344a1c1dc46e5784b7542bf28fc3d9
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 17:01:12 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -4,7 +4,7 @@ import plotly.express as alpha
 import plotly
 import array as arr
 c=10
-a = 0.1*np.ones((c,c))
+a = 0.0*np.ones((c,c))
 beam = np.ones((c,c))
 a[0][0]=0.5
 b = np.arange(c,0,-1)
@@ -18,7 +18,7 @@ for z in range(0,int(c/2),1):
   for y in range(int(c)-1,int(c/2)-1,-1):
     i=int(y)
     j=int(z)
-    a[j][i]=0.9
+    a[j][i]=1.0 # alpha max
 i=0
 j=0
 for z in range(int(c/2),-int(c/2),-1):
@@ -27,6 +27,6 @@ for z in range(int(c/2),-int(c/2),-1):
     j=int(z)
     beam[j][i]=((i+j)/c)**2
 rayf=a*beam;
-fig = alpha.imshow(rayf,text_auto=True)
+fig = alpha.imshow(beam,text_auto=True)
 fig.show()
 #plotly.offline.plot(fig, filename='alpha.html')
