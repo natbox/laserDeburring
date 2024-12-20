bf7a4d017d6b42889695d04c3923a20880e9aa0b
natbox <179704619+natbox@users.noreply.github.com>
2024-12-20 17:41:22 +0100

    Update vofLaser.py

--- vofLaser.py
+++ vofLaser.py
@@ -25,9 +25,10 @@ for z in range(int(c/2),-int(c/2),-1):
   for y in range(-int(c/2),int(c/2),1):
     i=int(y)
     j=int(z)
-    beam[j][i]=(3*(i**2+j**2)/c**2)
+    beam[j][i]=(3*(i**2+j**2)/c**3)
     beam[j][i]=beam[j][i]/beam.max()
 rayf=a*beam;
-fig = alpha.imshow(rayf,text_auto=True)
+beam=100*beam;
+fig = alpha.imshow(beam,text_auto=True)
 fig.show()
 #plotly.offline.plot(fig, filename='alpha.html')
